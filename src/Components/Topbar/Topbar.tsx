38d972993328bcb01946de7ed85ff96184fa9852
kolvra <sabrisirajkholbiabby@gmail.com>
2025-09-11 10:38:12 +0700

    refactor(assets): move images to src/assets and update imports

--- src/Components/Topbar/Topbar.tsx
+++ src/Components/Topbar/Topbar.tsx
@@ -1,14 +1,19 @@
 import Searchbox from "./Searchbox";
 import ButtonTopbar from "./ButtonTopbar";
+import { bellIcon } from "../../assets";
+
 function Topbar() {
   return (
     <div className="w-full h-20 flex justify-between items-center p-5 size-full">
       <div className="w-90 h-10">
-        <Searchbox label="Search product, supplier, order"/>
+        <Searchbox label="Search product, supplier, order" />
       </div>
       <div className="flex flex-row gap-5 items-center">
-        <ButtonTopbar image="../../../../public/images/bell.svg" alt="bell notification icon"/>
-        <ButtonTopbar alt="photo profile"/>
+        <ButtonTopbar
+          image={bellIcon}
+          alt="bell notification icon"
+        />
+        <ButtonTopbar alt="photo profile" />
       </div>
     </div>
   );
---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import bellIcon from "./bell.svg"
+import logo from "./logo.svg"
+import magnifyingGlassIcon from "./magnifying-glass.svg"
+
+export {
+    bellIcon,
+    logo,
+    magnifyingGlassIcon
+}
